9ef8cea7285f4bf03ed1dd866b1b32e3a5978d8b
Chris King <ccking@smu.edu>
2012-06-11 19:30:57 -0500

    Fixed scope problem, GPS problem, and concurrency issue

--- parking_onx.js
+++ parking_onx.js
@@ -3,14 +3,19 @@
  *
  * Nick Barone
  * @nbar1
+ * Edited by Chris King
+ * @raikia
  */
 
+ var longitude_source = null;
+ var latitude_source = null;
+ 
 device.modeOfTransport.on('changed', function(signal)
 {
 	// Check to see if user went from 'driving' to 'atrest'
 	// Might have to write this to check for 'driving' > 'atrest' with (x to y seconds in between)
 	// then 'atrest' > 'walking' - or maybe listeners for each event.
-	if (signal.current === 'atrest' && signal.previous == 'driving')
+	if ((signal.current === 'atrest' || signal.current === 'walking') && signal.previous === 'driving')
 	{
 		
 		// Favorable signals caught
@@ -27,11 +32,15 @@ device.modeOfTransport.on('changed', function(signal)
 			var listener = device.location.createListener('GPS', 5000);
 			listener.on('changed', function (signal)
 			{
-				var longitude_source = signal.location.longitude;
-				var latitude_source = signal.location.latitude;
+				longitude_source = signal.location.longitude;
+				latitude_source = signal.location.latitude;
 				listener.stop();
 			});
+			listener.start();
+			while (longitude_source === null || latitude_source === null) { }
 			device.browser.launch("http://maps.google.com/maps?saddr="+latitude_source+","+longitude_source+"&daddr="+latitude_dest+","+longitude_dest);
+			longitude_source = null;
+			latitude_source = null;
 		});
 	}
 });
